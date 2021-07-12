fb8a26c65f26c34caea0624dd40a0a5abcadf6f4
Pedro Guilherme <64043606+Pedrogui29@users.noreply.github.com>
2021-07-12 13:37:08 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import {ChatEngine} from 'react-chat-engine';
+import { ChatEngine } from 'react-chat-engine';
 
 import ChatFeed from './components/ChatFeed';
-
+import LoginForm from './components/LoginForm';
 import './App.css';
 
+const projectID = '95e95ded-0254-434d-ae2b-ab3c9296eeff';
 
 const App = () => {
+  if (!localStorage.getItem('username')) return <LoginForm />;
 
-    return (
-  <ChatEngine
-      height= "100vh"
-      projectID="95e95ded-0254-434d-ae2b-ab3c9296eeff"
-      userName="pedrogui"
-      userSecret="29102002p"
-      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps}/>}
-
-  />
-    );
-}
-
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+    />
+  );
+};
 export default App;
